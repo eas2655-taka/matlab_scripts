545bf88a847a93649e6817aeca2a08c471c648a8
Taka Ito <taka.ito@eas.gatech.edu>
2018-01-22 11:42:32 -0500

    0122v0

--- week2_exercise.m
+++ week2_exercise.m
@@ -21,3 +21,36 @@ colorbar ;
 xlabel('longitude');
 ylabel('latitude');
 title('surface air temperature , degree C');
+
+% include m_map in the path (this is specific to where you saved m_map)
+addpath m_map1.4/m_map
+
+% global equal-area projection, hammer-aitoff
+figure(3);
+m_proj('hammer-aitoff','clongitude',-150); % define projection
+lon(end+1)=lon(1)+360;                     % wrap around the globe
+air(end+1,:,:)=air(1,:,:);
+m_pcolor(lon,lat,air(:,:,end)');
+hold on;
+m_pcolor(lon-360,lat,air(:,:,end)');       % plot twice for western hemis.
+shading flat;
+colormap('jet');
+colorbar;
+m_coast;
+m_grid('xaxis','middle');
+xlabel('longitude');
+ylabel('latitude');
+
+% polar projection, stereographic, with m_contourf, m_contour or m_pcolor
+figure(4)
+m_proj('stereographic','latitude',90,'longitude',0,'radius',60); 
+%m_pcolor(lon,lat,air(:,:,end)');
+%m_contour(lon,lat,air(:,:,end)');
+m_contourf(lon,lat,air(:,:,end)');
+hold on;
+shading flat;
+colormap('jet');
+colorbar;
+m_coast;
+m_grid('xaxis','bottom');
+
