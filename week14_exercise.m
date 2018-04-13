aeed6a11e57c87ad108c470e9a943bb8a1d0ca1f
eas2655-taka <35376793+eas2655-taka@users.noreply.github.com>
2018-04-13 10:58:09 -0400

    Add files via upload

---
+++ week14_exercise.m
@@ -0,0 +1,42 @@
+%% week14_exercise.m 
+
+% safety first
+close all;
+clear all;
+
+% set up domain
+L = 10; % domain size
+dx= .2; % grid spacing
+x = [0:dx:L]';
+Nx= length(x);
+
+% model parameters
+k  = 1e-2; % diffusivity
+T0 = 10;   % initial condition
+
+% loop for time evolution
+dt = 1;
+Nt = 1000;
+
+for t=1:Nt
+    % prepare array for T
+    T = zeros(Nx,1);
+    time(t) = dt*(t-1);
+    
+    % Fourier series
+    for n=1:2:201
+        T = T + (4*T0)/(n*pi)*sin(n*pi*x/L)*exp(-(n*pi/L)^2*k*time(t));
+    end
+    
+    % plot temperature
+    figure(1);
+    plot(x,T,'k-');
+    xlabel('distance in x');
+    ylabel('temperature');
+    axis([0 L 0 T0+3]);
+    title( ['time=',num2str(time(t))] );
+    drawnow;
+    
+end
+
+
