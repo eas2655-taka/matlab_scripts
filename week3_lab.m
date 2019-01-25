c304799bf72dfd51356ec8fe8d9238bcb41ed4ed
eas2655-taka <35376793+eas2655-taka@users.noreply.github.com>
2019-01-25 14:45:14 -0500

    Add files via upload

--- week3_lab.m
+++ week3_lab.m
@@ -1,57 +1,59 @@
-%% week 3 lab
+%% week3_lab
 
-close all;
-clearvars;
+% Taka Ito
 
-% display meta data
-%ncdisp('air.mon.mean.nc');
+% safety first
+close all
+clear all
+clc
 
-%% load in monthly mean surface air temp data
-lat=ncread('air.mon.mean.nc','lat');
-lon=ncread('air.mon.mean.nc','lon');
-time=ncread('air.mon.mean.nc','time');
-time=time/24/365+1800;
-air=ncread('air.mon.mean.nc','air');
+% 1. load the data
+load atlanta_temperature.mat;
 
-%% extract temperature at equator
-J=find(lat==0);   % find index for lat=0
-Teq=air(:,J,:);
-Teqm=mean(Teq,1); % average all longitude
-Teqm=squeeze(Teqm);
+% 2. long term mean
+mu = mean(Jul);
+disp(['The long term mean July temperature is :',num2str(mu)]);
 
-figure(1);
-plot(time,Teqm);  % take a look at the data 
+% 3. 2000-2018 mean
+I = find(Year>=2000);
+X = Jul(I); % extract July temperature after 2000 
+Xave = mean(X); % take the mean
+disp('---------')
+disp(['The 2000-2018 mean July temperature is :',num2str(Xave)]);
 
-% calculate annual mean
-Teq_matrix = reshape(Teqm(1:68*12),12,68); % set up a matrix
-Teq_annual = mean(Teq_matrix,1); % take averages of 12 months
-year = 1948:2015; % set up new time axis
+% 4. 5 steps 
+% (1) state the confidence level
+CL = 0.99;
+disp('---------')
+disp(['The confidence level is ',num2str(CL*100),' percent. ']);
 
-hold on;
-plot(year,Teq_annual,'r-');
+% (2) define null hypothesis and its alternative
+disp('---------')
+disp('H0 (Null): 2000-2018 mean July temp. is not significantly different from the long-term mean.');
+disp('H1 (Alternative): 2000-2018 mean July temp. is significantly warmer than long-term mean.'); 
 
-%% evaluate the 2005-2015 mean temperature
+% (3) state the statistics used
+disp('---------')
+disp('We use Students t distribution and perform two-tail test.');
 
-% 1. Confidence level
-CL = .99;
+% (4) calculate the critical region
+N=19;
+df=N-1;
+tcrit = tinv((1+CL)/2,df);
+disp('---------')
+disp(['The critical t-value is :',num2str(tcrit)]);
 
-% 2. Null hypothesis and alternate hypothesis
-% H0: the 2005-2015 mean is not different from long term mean
-% H1: the 2005-2015 mean is warmer than long term mean
-mu = mean(Teq_annual)
-J = find(year>=2005);
-X = Teq_annual(J);
-Xave = mean(X)
-Xstd = std(X)
-N = length(X)
+% (5) evaluate whether or not the data is within the critical region
+s = std(X); % sample STD
+t = (Xave-mu)/(s/sqrt(N-1));
+disp('---------')
+disp(['The t-value of the data is :',num2str(t)]);
 
-% 3. use Student's t-dist with two tail test
-
-% 4. calculate critical region
-tcrit = tinv(.5*(CL+1),N-1)
-
-% 5. evaluate the hypothesis
-tval = (Xave-mu)/(Xstd/sqrt(N-1))
-
-% The data rejects H0. 2005-2015 temperature is significantly warmer. 
+if abs(t) > tcrit
+   disp(['|t| > tcrit: the null hypothesis is rejected. ']);
+   %2000-2018 july is hotter than long term mean
+else
+   disp(['|t| < tcrit: the null hypothesis is not rejected. '])
+   %2000-2018 july is not significantly different from the long term mean
+end
 
