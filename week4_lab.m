2885056694c8b6a50e9edbfa686ef66b40f4d1c1
eas2655-taka <35376793+eas2655-taka@users.noreply.github.com>
2020-01-31 14:49:28 -0500

    Add files via upload

---
+++ week4_lab.m
@@ -0,0 +1,36 @@
+%% week4 lab
+
+close all
+clear all
+
+% load the data
+load air.mon.mean.mat;
+
+% annual mean land temp
+lndtempam=mean(ocntemp,4);
+
+% calculate regression coefficients
+N = size(lndtempam);
+x = yr(:,1);
+% loop over all grid points
+for i=1:N(1)
+    for j=1:N(2)
+        y=squeeze(lndtempam(i,j,:));
+        [a,b,r2]=regr(x,y);
+        % record the regression coeffs
+        A(i,j)=a;
+        B(i,j)=b;
+        R2(i,j)=r2;
+    end
+end
+
+figure(1);
+pcolor(A'); % 2D color plot
+shading flat; % remove grid lines
+set(gca,'Ydir','rev'); % invert the y axis
+colormap('jet'); % set color scheme
+colorbar; % add color bar
+caxis([-.1 .1]); % set the max/min color shading
+
+
+
