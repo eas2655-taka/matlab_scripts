c84e609a9479534ca5c574fff473754356333781
eas2655-taka <35376793+eas2655-taka@users.noreply.github.com>
2020-02-05 14:48:17 -0500

    Add files via upload

--- week5.m
+++ week5.m
@@ -1,44 +1,53 @@
+%% week5
+
 close all
 clear all
 
-load atlanta_temperature.mat
-year_atlanta = Year;
-temp_atlanta = Annual;
+% set time range
+range=[1940 2018];
 
-load Athens_temperature.mat
-year_athens = year;
-temp_athens = annual;
+%load data
+load atlanta_temperature.mat;
+time1=Year;
+temp1=Annual;
+I=find(time1>=range(1)&time1<=range(2));
+time1=time1(I);
+temp1=temp1(I);
 
-load Boulder_temperature.mat
-year_boulder = year;
-temp_boulder = annual;
+%load data
+load boulder_temperature.mat;
+time2=year1;
+temp2=annual;
+I=find(time2>=range(1)&time2<=range(2));
+time2=time2(I);
+temp2=temp2(I);
 
-% set time period
-range = [1940 2010];
-
-% x = atlanta temp, y = athens temp
-I=find(year_atlanta>=range(1)&year_atlanta<=range(2));
-x = temp_atlanta(I);
-
-I=find(year_athens>=range(1)&year_athens<=range(2));
-y = temp_athens(I);
-
-I=find(year_boulder>=range(1)&year_boulder<=range(2));
-z = temp_boulder(I);
-
-% quick look at the data
-figure(1);
-p(1)=plot(range(1):range(2),x,'b-');
-hold on;
-p(2)=plot(range(1):range(2),y,'m-');
-p(3)=plot(range(1):range(2),z,'r-');
-hold off;
+% quick look
+figure(1)
+p(1)=plot(time1,temp1,'k-');
 xlabel('time');
-ylabel('temperature, deg F');
-legend(p,{'Atlanta' 'Athens' 'Boulder'});
+ylabel('temperature, degF');
+hold on;
+p(2)=plot(time2,temp2,'r-');
+hold off;
+legend(p,{'Atlanta' 'Boulder'});
 
+% calculate correlation coefficient
+c=cov(temp1,temp2);
+r=c(1,2)/sqrt(c(1,1)*c(2,2));
+disp(['r value is : ',num2str(r)]);
 
-
+% Testing the significance
+CL=95; % 1. set CL
+% H0: there is no significant positive correlation
+% H1: there is significant positive correlation
+% 3. t-test, 1 tail 
+% 4. confidence interval
+N=length(temp1);
+df=N-2;
+tcrit=tinv(CL/100,df);
+% 5. Evaluate the hypothesis with the data
+t = r*sqrt((N-2)/(1-r*r));
 
 
 
